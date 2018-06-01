c4304e5ce70a86808222f465baae80bdb9891725
Squareys <squareys@googlemail.com>
2018-06-01 15:37:56 +0200

    template: Handle notifications while waiting for responses
    Signed-off-by: Squareys <squareys@googlemail.com>

--- template/neovimapi.cpp
+++ template/neovimapi.cpp
@@ -1,11 +1,14 @@
 // Auto generated {{date}} from nvim API level:{{api_level}}
 #include "neovimapi{{api_level}}.h"
 
+#include <type_traits>
 
 namespace NeovimApi {
 
 using namespace Corrade;
 using namespace Magnum;
+
+const char** getNotificationTypeStrings();
 
 void mpack_write(mpack_writer_t* writer, const Vector2i& val) {
     mpack_write_i32(writer, val.x());
@@ -71,48 +74,46 @@ Object mpack_read_object(mpack_reader_t* reader) {
     return result;
 }
 
-Containers::Array<std::string> mpack_read_array_of_str(mpack_reader_t* reader) {
-    const size_t numElements = mpack_expect_array(reader);
-
-    Containers::Array<std::string> data{Containers::DefaultInit, numElements};
-
-    char buffer[512];
-    for(int i = 0; i < numElements; ++i) {
-        mpack_expect_cstr(reader, buffer, sizeof(buffer));
-        data[i] = std::string(buffer);
-    }
-
-    mpack_done_array(reader);
-    return data;
+template<typename T, typename... Args>
+void mpack_write(mpack_writer_t* writer, T value, Args... args) {
+    mpack_write(writer, value);
+    mpack_write(writer, args...);
 }
 
-Containers::Array<Long> mpack_read_array_of_long(mpack_reader_t* reader) {
-    const size_t numElements = mpack_expect_array(reader);
+void mpack_write(mpack_writer_t* writer) {}
 
-    Containers::Array<Long> data{Containers::DefaultInit, numElements};
+template<typename T> T mpack_read(mpack_reader_t* reader);
 
-    for(int i = 0; i < numElements; ++i) {
-        data[i] = mpack_expect_i64(reader);
-    }
-
-    mpack_done_array(reader);
-    return data;
+template<>
+bool mpack_read<bool>(mpack_reader_t* reader) {
+    return mpack_expect_bool(reader);
 }
-
-Containers::Array<Object> mpack_read_array_of_object(mpack_reader_t* reader) {
-    const size_t numElements = mpack_expect_array(reader);
-
-    Containers::Array<Object> data{Containers::DefaultInit, numElements};
-
-    for(int i = 0; i < numElements; ++i) {
-        data[i] = mpack_read_object(reader);
-    }
-
-    mpack_done_array(reader);
-    return data;
+template<>
+Long mpack_read<Long>(mpack_reader_t* reader) {
+    return mpack_expect_i64(reader);
 }
-
-std::unordered_map<std::string, Object>&& mpack_read_map(mpack_reader_t* reader) {
+template<>
+Double mpack_read<Double>(mpack_reader_t* reader) {
+    return mpack_expect_double(reader);
+}
+template<>
+std::string mpack_read<std::string>(mpack_reader_t* reader) {
+    char strBuf[512];
+    mpack_expect_cstr(reader, strBuf, sizeof(strBuf));
+    return strBuf;
+}
+template<>
+Object mpack_read<Object>(mpack_reader_t* reader) {
+    return mpack_read_object(reader);
+}
+template<>
+Vector2i mpack_read<Vector2i>(mpack_reader_t* reader) {
+    const Int x = mpack_expect_i32(reader);
+    const Int y = mpack_expect_i32(reader);
+    return {x, y};
+}
+template<>
+std::unordered_map<std::string, Object> mpack_read<std::unordered_map<std::string, Object>>(mpack_reader_t* reader) {
     const size_t numElements = mpack_expect_map(reader);
 
     std::unordered_map<std::string, Object> options{numElements};
@@ -124,7 +125,32 @@ std::unordered_map<std::string, Object>&& mpack_read_map(mpack_reader_t* reader)
     }
 
     mpack_done_map(reader);
-    return std::move(options);
+    return options;
+}
+
+template<typename T>
+Containers::Array<T> mpack_read_array(mpack_reader_t* reader) {
+    const size_t numElements = mpack_expect_array(reader);
+
+    Containers::Array<T> data{Containers::DefaultInit, numElements};
+    for(int i = 0; i < numElements; ++i) {
+        data[i] = mpack_read<T>(reader);
+    }
+
+    mpack_done_array(reader);
+    return data;
+}
+template<>
+Containers::Array<Long> mpack_read<Containers::Array<Long>>(mpack_reader_t* reader) {
+    return mpack_read_array<Long>(reader);
+}
+template<>
+Containers::Array<Object> mpack_read<Containers::Array<Object>>(mpack_reader_t* reader) {
+    return mpack_read_array<Object>(reader);
+}
+template<>
+Containers::Array<std::string> mpack_read<Containers::Array<std::string>>(mpack_reader_t* reader) {
+    return mpack_read_array<std::string>(reader);
 }
 
 NeovimApi{{api_level}}::NeovimApi{{api_level}}(int port, int receiveBufferSize):
@@ -138,81 +164,124 @@ NeovimApi{{api_level}}::NeovimApi{{api_level}}(const std::string& host, int port
 {
 }
 
-{% for f in functions %}
-
-{{f.return_type.native_type}} NeovimApi{{api_level}}::{{f.name}}({{f.argstring}}) {
+template<typename... Args>
+Int NeovimApi{{api_level}}::dispatch(const std::string& func, Args... args) {
     char* data;
     size_t size;
     mpack_writer_t writer;
     mpack_writer_init_growable(&writer, &data, &size);
 
+    const int msgId = nextMessageId();
     mpack_start_array(&writer, 4);
     mpack_write_i32(&writer, 0);
-    mpack_write_i32(&writer, 0);
-    mpack_write_cstr(&writer, "{{f.name}}");
+    mpack_write_i32(&writer, msgId);
+    mpack_write_cstr(&writer, func.c_str());
 
-    mpack_start_array(&writer, {{f.argcount}});
-{% for p in f.parameters %}
-{% if p.native_type == "Long" %}
-    mpack_write_i64(&writer, {{p.name}});
-{% elif p.native_type == "bool" %}
-    mpack_write_bool(&writer, {{p.name}});
-{% else %}
-    mpack_write(&writer, {{p.name}});
-{% endif %}
-{% endfor %}
+    mpack_start_array(&writer, sizeof...(Args));
+    mpack_write(&writer, args...);
+
     mpack_finish_array(&writer);
     mpack_finish_array(&writer);
-    CORRADE_ASSERT(mpack_writer_destroy(&writer) == mpack_ok, "NeovimApi{{api_level}}::{{f.name}}(): Could not pack request", {{"{}" if f.return_type.native_type != "void" else ""}});
+    CORRADE_ASSERT(mpack_writer_destroy(&writer) == mpack_ok, "NeovimApi{{api_level}}::dispatch(): Could not pack request", -1);
 
     _socket.send(Containers::arrayView(data, size));
-    Containers::ArrayView<char> response = _socket.receive(_receiveBuffer);
+    return msgId;
+}
 
-    mpack_reader_t reader;
-    mpack_reader_init_data(&reader, response.data(), response.size());
+void NeovimApi{{api_level}}::handleNotification(mpack_reader_t& reader) {
+    const NotificationType func = (NotificationType)mpack_expect_enum(&reader, getNotificationTypeStrings(), Int(NotificationType::Count));
 
-    mpack_expect_array_max(&reader, 4);
-    const Int type = mpack_expect_i32(&reader);
-    const UnsignedInt msgid = mpack_expect_u32(&reader);
-    const mpack_tag_t error = mpack_peek_tag(&reader);
-    if(error.type != mpack_type_nil) {
-        // TODO: An error occurred...
-    }
-    mpack_read_tag(&reader);
-    const mpack_tag_t result = mpack_peek_tag(&reader);
+    /* Get parameter binary data */
+    const char* remainingBuffer;
+    const size_t remainingCount = mpack_reader_remaining(&reader, &remainingBuffer);
+    Notification n{func, Containers::Array<char>{Containers::NoInit, remainingCount}};
+    std::copy_n(remainingBuffer, remainingCount, n.parameters.data());
+    _notifications.push_back(std::move(n));
+}
 
-{% if f.return_type.neovim_type != "void" %}
-{% if f.return_type.neovim_type == "String" %}
-    char strBuf[512];
-    mpack_expect_cstr(&reader, strBuf, sizeof(strBuf));
-    std::string returnValue(strBuf);
-{% elif f.return_type.native_type == "Long" %}
-    Long returnValue = mpack_expect_i64(&reader);
-{% elif f.return_type.native_type == "Double" %}
-    Double returnValue = mpack_expect_double(&reader);
-{% elif f.return_type.native_type == "bool" %}
-    bool returnValue = mpack_expect_bool(&reader);
-{% elif f.return_type.native_type == "Object" %}
-    Object returnValue = mpack_read_object(&reader);
-{% elif f.return_type.native_type == "Vector2i" %}
-    const Int x = mpack_expect_i32(&reader);
-    const Int y = mpack_expect_i32(&reader);
-    auto returnValue = Vector2i{x, y};
-{% elif f.return_type.neovim_type == "Dictionary" %}
-    auto returnValue = mpack_read_map(&reader);
-{% elif f.return_type.elemtype == "String" %}
-    Containers::Array<std::string> returnValue = mpack_read_array_of_str(&reader);
-{% elif f.return_type.native_elemtype == "Long" %}
-    Containers::Array<Long> returnValue = mpack_read_array_of_long(&reader);
-{% elif f.return_type.elemtype == "Object" %}
-    Containers::Array<Object> returnValue = mpack_read_array_of_object(&reader);
-{% endif %}
-    mpack_done_array(&reader);
-    CORRADE_ASSERT(mpack_reader_destroy(&reader) == mpack_ok, "NeovimApi{{api_level}}::{{f.name}}(): Could not unpack response", {});
+template<typename T>
+auto NeovimApi{{api_level}}::waitForResponse(Int msgId) {
+    do {
+        Containers::ArrayView<char> response = _socket.receive(_receiveBuffer);
+        // if(response.size() == 0 || response.data() == nullptr) ...
+        if(response.size() == _receiveBuffer.size()) {
+            Warning() << "NeovimApi{{api_level}}::waitForResponse(): Receive buffer was full";
+            // TODO: Handle requests over multiple buffers
+        }
 
-    return returnValue;
+        mpack_reader_t reader;
+        mpack_reader_init_data(&reader, response.data(), response.size());
+
+        mpack_expect_array_max(&reader, 4);
+        const MessageType type = MessageType(mpack_expect_i32(&reader));
+
+        if(type == MessageType::Notification) {
+            handleNotification(reader);
+        } else if(type == MessageType::Response) {
+            if(mpack_expect_u32(&reader) != msgId) {
+                Warning() << "NeovimApi{{api_level}}::waitForResponse(): Skipped a response that was not waited for.";
+                continue;
+            }
+            const mpack_tag_t error = mpack_peek_tag(&reader);
+            if(error.type != mpack_type_nil) {
+                if(error.type == mpack_type_str) {
+                    char strBuf[512];
+                    mpack_expect_cstr(&reader, strBuf, sizeof(strBuf));
+                    std::string errorMessage(strBuf);
+                    if constexpr (std::is_void<T>::value) {
+                        CORRADE_ASSERT(false, "NeovimApi{{api_level}}::waitForResponse(): " + errorMessage, );
+                    } else {
+                        CORRADE_ASSERT(false, "NeovimApi{{api_level}}::waitForResponse(): " + errorMessage, T{});
+                    }
+                }
+            }
+            mpack_discard(&reader);
+            const mpack_tag_t result = mpack_peek_tag(&reader);
+
+            if constexpr (std::is_void<T>::value) {
+                CORRADE_ASSERT(result.type == mpack_type_nil,
+                        "NeovimApi{{api_level}}::waitForResponse(): Expected nil return, but got " + std::to_string(result.type), );
+            } else {
+                T returnValue = mpack_read<T>(&reader);
+                mpack_done_array(&reader);
+                CORRADE_ASSERT(mpack_reader_destroy(&reader) == mpack_ok,
+                        "NeovimApi{{api_level}}::waitForResponse(): Could not unpack response", T{});
+
+                return std::forward<T>(returnValue);
+            }
+        } else if(type == MessageType::Request) {
+            mpack_print(response.data(), response.size());
+            const UnsignedInt msgid = mpack_expect_u32(&reader);
+            char strBuf[512];
+            mpack_expect_cstr(&reader, strBuf, sizeof(strBuf));
+            Error() << "Received request" << std::string(strBuf) << "Server -> Client requests are not implemented.";
+        } else {
+            Error() << "Unknown message type" << Int(type);
+        }
+    } while(true);
+}
+
+static const char* NotificationTypeStrings[]{
+{% for n in notifications %}
+    "{{ n.name }}",
+{% endfor %}
+};
+
+const char** getNotificationTypeStrings() {
+    return NotificationTypeStrings;
+}
+
+
+{% for f in functions %}
+
+{{f.return_type.native_type}} NeovimApi{{api_level}}::{{f.name}}({{f.argstring}}) {
+{% if f.parameters %}
+    const Int msgId = dispatch("{{f.name}}", {{ f.parameters|join(", ", attribute='name') }});
 {% else %}
-    CORRADE_ASSERT(result.type == mpack_type_nil, "NeovimApi{{api_level}}::{{f.name}}(): Expected nil return, but got " + std::to_string(result.type), );
+    const Int msgId = dispatch("{{f.name}}");
+{% endif %}
+{% if f.return_type.native_type != "void" %}
+    return waitForResponse<{{f.return_type.native_type}}>(msgId);
 {% endif %}
 }
 {% endfor %}
--- template/neovimapi.h
+++ template/neovimapi.h
@@ -7,23 +7,112 @@
 #include "mpack/mpack.h"
 #include "Corrade/Net/Socket.h"
 
+#include <Corrade/Containers/Array.h>
+#include <Corrade/Containers/ArrayView.h>
+
 #include <Magnum/Magnum.h>
 #include <Magnum/Math/Vector2.h>
 
+#include <vector>
+
 namespace NeovimApi {
 
-typedef mpack_tag_t Object;
+using Magnum::Int;
 using Magnum::Long;
 using Magnum::Vector2i;
 
+enum class MessageType: Int {
+    Request = 0,
+    Response = 1,
+    Notification = 2
+};
+
+enum class NotificationType: Int {
+{% for n in notifications %}
+    {{ n.name }},
+{% endfor %}
+    Count
+};
+
+/**
+@brief A multi-type storage
+
+Inefficient implementation of a variadic, stores either a
+string, bool or Long.
+*/
+struct Object {
+    /** Indicates which member is valid */
+    mpack_type_t type;
+    union {
+        bool b;
+        Long l;
+    };
+    std::string s;
+};
+
+/**
+@brief A msgpack-rpc notification
+
+A message from the server to the client.
+*/
+struct Notification {
+    /** Method called by the server */
+    NotificationType method;
+    /** Parameters, binary data in msgpack format, the parameters array. */
+    Corrade::Containers::Array<char> parameters;
+};
+
+/**
+@brief A neovim API client
+
+Allows dispatching msgpack-rpc requests to a neovim instance via TCP and then
+receive notification and responses back.
+
+Warning: Nothing is thread safe here at the moment!
+*/
 class NeovimApi{{api_level}} {
 public:
 
     /** @brief Constructor to connect to a local neovim */
-    NeovimApi{{api_level}}(int port, int receiveBufferSize=512);
+    NeovimApi{{api_level}}(int port, int receiveBufferSize=2048*32);
 
     /** @brief Constructor to connect to neovim */
-    NeovimApi{{api_level}}(const std::string& host, int port, int receiveBufferSize=512);
+    NeovimApi{{api_level}}(const std::string& host, int port, int receiveBufferSize=2048*32);
+
+    /**
+     * @brief Dispatch a msgpack-rpc request
+     * @param func Name of the function to call
+     * @param args Arguments to the function
+     * @return An automatically incremented msgId to use with @ref waitForResponse()
+     */
+    template<typename... Args>
+    Int dispatch(const std::string& func, Args... args);
+
+    /**
+     * @brief Wait for a reponse to a dispatched request with given id
+     * @param msgId Message id to wait for a response to
+     * @return The return value from the response
+     *
+     * This method will save all encountered notifications for later.
+     * See @ref pollNotifications().
+     */
+    template<typename T>
+    auto waitForResponse(Int msgId);
+
+    /**
+     * @brief Poll pending notifications
+     * @return array of notifications
+     *
+     * Copies currently pending notifications and then clears pending notifications.
+     */
+    Corrade::Containers::Array<Notification> pollNotifications() {
+        Corrade::Containers::Array<Notification> notifications(Corrade::Containers::DefaultInit, _notifications.size());
+        for(int i = 0; i < _notifications.size(); ++i) {
+            notifications[i] = std::move(_notifications[i]);
+        }
+        _notifications.clear();
+        return notifications;
+    }
 
 {% for f in functions %}
 
@@ -38,6 +127,13 @@ private:
 
     Corrade::Net::Socket _socket;
     Corrade::Containers::Array<char> _receiveBuffer;
+    std::vector<Notification> _notifications;
+
+    int _nextMessageId = 0;
+    /* Generate the next message id */
+    int nextMessageId() { return _nextMessageId++; }
+
+    void handleNotification(mpack_reader_t& reader);
 };
 
 }
